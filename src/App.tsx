e7bef6c53e908c6319a9963f2df6e8276910eb72
xsadia <lipearthur@hotmail.com>
2022-02-01 00:53:05 -0300

    :monkey:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import styled from "styled-components";
 import { Layout } from "./components/Layout";
+import { LeftPanel } from "./components/LeftPanel";
+import { Section } from "./components/Section";
+
+const TestContainer = styled.div`
+  height: 360px;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <Layout>
-      <h1>Hello, World!</h1>
+      <Section>
+        <LeftPanel />
+        <TestContainer>
+          <h1>SECTION</h1>
+        </TestContainer>
+      </Section>
     </Layout>
   );
 }
